f0659853c01a12e88822ae1194857f075c9438e6
LeoRocha <leonardocar.dm@gmail.com>
2019-04-27 18:36:30 -0400

    Change test file to support strange cases. Change initial print in Main. Add preprocessor implementation to remove multi-line comments - working but need refactoring.

--- src/java/Main.java
+++ src/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
 
         File directory = new File("../../bin/test.xpp");
-        System.out.println(directory.getAbsolutePath());
+        System.out.println("Test file: " + directory.getAbsolutePath());
         Preprocessor preprocessor = new Preprocessor();
 
         try {
--- src/java/Preprocessor.java
+++ src/java/Preprocessor.java
@@ -20,24 +20,19 @@ class Preprocessor {
      * @param lineNumberReader file reference.
      */
     void preProcess(LineNumberReader lineNumberReader) throws IOException {
-        String line;
+        String currentLine;
         output = new String[MAX_BUFFER_SIZE];
         int outputLineIndex = 0;
-        boolean skipLine = false;
 
-        while ((line = lineNumberReader.readLine()) != null) {
-            if (isASingleLineComment(line)) {
-                System.out.println("Found a single-line comment at line " + lineNumberReader.getLineNumber());
-                skipLine = true;
-            }
-            if (isAMultiLineComment(lineNumberReader, line)) {
-                //TODO complete method.
-            }
-
-            if (!skipLine)
-                output[outputLineIndex++] = line;
-
-            skipLine = false;
+        while ((currentLine = lineNumberReader.readLine()) != null) {
+            if (isAMultiLineComment(currentLine)) {
+                String[] splitMultiLineComment = splitMultiLineComment(lineNumberReader, currentLine);
+                for (String line : splitMultiLineComment) {
+                    if (line != null && !line.isEmpty())
+                        output[outputLineIndex++] = line;
+                }
+            } else if (isNotASingleLineComment(currentLine))
+                output[outputLineIndex++] = currentLine;
         }
         output[outputLineIndex] = "EOF";
         printOutput();
@@ -55,34 +50,52 @@ class Preprocessor {
     }
 
     /**
-     * @param line current line being preprocessed.
+     * @param currentLine last read line..
      * @return true if the line is a single-line comment.
      */
-    private static boolean isASingleLineComment(String line) {
-        return line.contains("//");
+    private static boolean isNotASingleLineComment(String currentLine) {
+        return !currentLine.contains("//");
     }
 
     /**
-     * @param line
-     * @param currentLine
-     * @return true if we found a multi-line comment.
+     * @param currentLine last read line.
+     * @return true if the line marks a Multi-line comment.
      */
-    private static boolean isAMultiLineComment(LineNumberReader lineNumberReader, String currentLine) {
-        if(currentLine.contains("/*")){
-            //try to match with another */
-            String separator = "/" + "*";
-            if(currentLine.contains("*/")) {
-                String[] splitStrings = currentLine.split(separator);
-                System.out.println(splitStrings[0]);
-                System.out.println(splitStrings[1]);
-            }
-            else{
-                //search for the matching */ in another line
-                while(!(currentLine = lineNumberReader.readLine()).contains("*/")){
-                    //found the line that contains the matching */
-                }
-            }
-        }
-        return  true;
+    private static boolean isAMultiLineComment(String currentLine) {
+        return currentLine.contains("/*");
     }
+
+    private static String[] splitMultiLineComment(LineNumberReader lineNumberReader, String currentLine) throws IOException {
+        int linesDistance = 0;
+        String[] output = new String[2];
+
+        String[] splitStrings = currentLine.split("/\\*");
+        if(splitStrings.length > 0)
+            output[0] = splitStrings[0];
+        else
+            output[0] = "";
+        //search for the matching */
+        while (!currentLine.contains("*/")) {
+            currentLine = lineNumberReader.readLine();
+            linesDistance++;
+        }
+        //found the line that contains the matching */
+        if (linesDistance == 0) {
+            splitStrings = splitStrings[1].split("\\*/");
+            if(splitStrings.length > 1)
+                output[0] += "" + splitStrings[1];
+            else
+                output[0] += "";
+        } else {
+            splitStrings = currentLine.split("\\*/");
+            if (splitStrings.length > 1)
+                output[1] = splitStrings[1];
+            else
+                output[1] = "";
+        }
+
+        return output;
+    }
+
+
 }
